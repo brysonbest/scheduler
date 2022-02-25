9a4dbb20f666db488cba9becdc0110689933d1b6
brysonbest <brycekalden@gmail.com>
2022-02-24 17:03:34 -0800

    Add confirm, show, and status components.

--- stories/index.js
+++ stories/index.js
@@ -21,6 +21,12 @@ import Header from "components/Appointment/Header"
 
 import Empty from "components/Appointment/Empty"
 
+import Show from "components/Appointment/Show"
+
+import Confirm from "components/Appointment/Confirm"
+
+import Status from "components/Appointment/Status"
+
 storiesOf("Button", module)
   .addParameters({
     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
@@ -150,5 +156,7 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with Time", () => <Appointment time="12pm" />)
   .add("Header", () => <Header time="12pm" />)
-  .add("Empty", () => <Empty onAdd={action("onAdd")}/>);
-  
+  .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+  .add("Show", () => <Show student="Lydia Miller-Jones" interviewer={interviewer} onEdit={action("onEdit")} onDelete={action("onDelete")}/>)
+  .add("Confirm", () => <Confirm message="Delete the Appointmnent?" onConfirm={action("onConfirm")} onCancel={action("onCancel")}/>)
+  .add("Status", () => <Status message="Deleting"/>);
