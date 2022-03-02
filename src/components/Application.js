021a0bd3aa176783749af927d8867633a317cb6c
brysonbest <brycekalden@gmail.com>
2022-03-01 18:13:14 -0800

    Implement api call for days and appointments from server.

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,58 +7,42 @@ import "components/Appointment"
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-const appointments = {
-  "1": {
-    id: 1,
-    time: "12pm",
-  },
-  "2": {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  "3": {
-    id: 3,
-    time: "2pm",
-  },
-  "4": {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  "5": {
-    id: 5,
-    time: "4pm",
-  }
-};
+import { getAppointmentsForDay } from "helpers/selectors";
+
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
+  const setDay = day => setState({ ...state, day});
 
   useEffect(() => {
-    axios.get("http://localhost:8001/api/days", {
+    Promise.all([
+      axios.get("http://localhost:8001/api/days", {
       proxy: {
         protocol: 'http',
         host: 'localhost',
         port: 8001
       }
-    }).then((response)=> {
-      setDays(response.data);
+    }),
+    axios.get("http://localhost:8001/api/appointments", {
+      proxy: {
+        protocol: 'http',
+        host: 'localhost',
+        port: 8001
+      }
+    }),
+    ]).then((all) => {
+      setState(prev => ({
+        ...prev, days: all[0].data, appointments: all[1].data
+      }))
+      console.log(all[0].data);
+      console.log(all[1].data);
     });
   }, []);
 
@@ -73,8 +57,8 @@ export default function Application(props) {
       <hr className="sidebar__separator sidebar--centered" />
       <nav className="sidebar__menu">
         <DayList
-          days={days}
-          value={day}
+          days={state.days}
+          value={state.day}
           onChange={setDay}
         />
       </nav>
@@ -86,7 +70,7 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {
-          Object.values(appointments).map(appointment => (
+          dailyAppointments.map(appointment => (
             <Appointment key={appointment.id} {...appointment}/>
           ))
         }
