f99e6e1153b81f4e68a74a9218af8cf44d76bfe1
brysonbest <brycekalden@gmail.com>
2022-03-01 18:42:12 -0800

    Add selector to get interviewer data for each appointment.

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,14 +7,15 @@ import "components/Appointment"
 import DayList from "./DayList";
 import Appointment from "components/Appointment";
 
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 
 export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: {}
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -37,12 +38,17 @@ export default function Application(props) {
         port: 8001
       }
     }),
+    axios.get("http://localhost:8001/api/interviewers", {
+      proxy: {
+        protocol: 'http',
+        host: 'localhost',
+        port: 8001
+      }
+    })
     ]).then((all) => {
       setState(prev => ({
-        ...prev, days: all[0].data, appointments: all[1].data
+        ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data
       }))
-      console.log(all[0].data);
-      console.log(all[1].data);
     });
   }, []);
 
@@ -70,9 +76,17 @@ export default function Application(props) {
       </section>
       <section className="schedule">
         {
-          dailyAppointments.map(appointment => (
-            <Appointment key={appointment.id} {...appointment}/>
-          ))
+          dailyAppointments.map((appointment) => {
+            const interview = getInterview(state, appointment.interview);
+            return (
+              <Appointment
+                key={appointment.id}
+                id={appointment.id}
+                time={appointment.time}
+                interview={interview}
+              />
+            );
+          })
         }
         <Appointment key="last" time="5pm" />
       </section>
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -14,3 +14,19 @@ export function getAppointmentsForDay(state, day) {
   }
   return dayArray;
 }
+
+
+export function getInterview(state, interview) {
+  if(!interview) {
+    return null;
+  }
+  const intObj = {
+    student: interview.student,
+    interviewer: {
+      id: interview.interviewer,
+      name: state.interviewers[interview.interviewer]['name'],
+      avatar: state.interviewers[interview.interviewer]['avatar']
+    }
+  }
+  return intObj;
+}
