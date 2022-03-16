59c5b28e66f5e0b8339feabcd91f176e660f9768
brysonbest <brycekalden@gmail.com>
2022-03-15 17:49:17 -0700

    Install and integrate propTypes checking.

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,8 +1,9 @@
 import React from "react";
 import InterviewerListItem from "./InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from "prop-types";
 
-export default function InterviewerList(props) {
+function InterviewerList(props) {
   return (
     <section className="interviewers">
       <h4 className="interviewers__header text--light">Interviewer</h4>
@@ -20,3 +21,9 @@ export default function InterviewerList(props) {
     </section>
   );
 }
+
+InterviewerList.propTypes = {
+  interviewers: PropTypes.array.isRequired,
+};
+
+export default InterviewerList;
