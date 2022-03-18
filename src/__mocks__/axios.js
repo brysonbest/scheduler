eb400580c4a461d3552ace9b1931ba9a985770dc
brysonbest <brycekalden@gmail.com>
2022-03-18 12:56:34 -0700

    Add testing for applications to load data, book interviews, and update spots remaining.

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -82,4 +82,12 @@ export default {
       });
     }
   }),
+  put: jest.fn(() => {
+    {
+      return Promise.resolve({
+        status: 204,
+        statusText: "No Content",
+      });
+    }
+  }),
 };
